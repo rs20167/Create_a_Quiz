79b2550b8a0fed7df40a696e4e70856dc9c936ed
rs20167 <110356426+rs20167@users.noreply.github.com>
2022-10-17 09:37:21 +1300

    Delete Score.py

--- Score.py
+++
@@ -1,9 +0,0 @@
-Win = "Correct +1"
-Lose = "Wrong is was"
-score = 8
-
-if score == 0:
-    print("WOW! you got 0 you really suck")
-else:
-
-    print("Good job you got {} out of 10 questions".format(score))
