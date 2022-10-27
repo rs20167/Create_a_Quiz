b74961c5c9c6fe16fe799d44068feba7a56599f2
rs20167 <110356426+rs20167@users.noreply.github.com>
2022-10-27 14:54:01 +1300

    Add files via upload

---
+++ Quiz 2.py
@@ -0,0 +1,161 @@
+# this adds points and tells you the right aner if you got it wrong
+Win = "Correct +1"
+Lose = "Wrong it was"
+score = 0
+birthday = 0
+# This ask for name
+name = input("What is your name? ")
+#This adds a system that calculates the users age by asking if they have had there bday then asking for there year of birth
+#then doing some maths to figure out there age
+yes_no = input("Have you have your birthday this year? ").lower()
+if yes_no == "yes" or yes_no == "y":
+    birthday =+ 2022
+else:
+    birthday =+ 2021
+
+def num_check(question, low, high):
+   error = "Please enter an  number between 1900 and 2022\n"
+
+   valid = False
+   while not valid:
+       try:
+           response = int(input(question))
+           if low < response <= high:
+               return response
+
+
+           else:
+               print(error)
+
+       except ValueError:
+           print(error)
+
+
+year = num_check("when were you born? ", 1900, 2022)
+
+age = birthday - year
+print("Your age is {}".format(age))
+
+
+
+
+
+
+
+#this a title for the quiz it tells the user what the quiz is about
+print("Welcome to my Fortnite quiz!")
+print()
+
+
+#this is a start button it tells the user to start
+start = input("Press enter to start...")
+if start == "":
+    print("")
+else:
+    print("")
+#The question is asked then the user has to answer them if they get it wrong it will tell you the right answer
+# or gives them 1 score if they get it right
+Q1 = input("What colour the rare rarity? ").lower()
+if Q1 == "blue":
+    print(Win)
+    score += 1
+
+else:
+    print("{} blue".format(Lose))
+
+print()
+Q2 = input("What colour is the Epic rarity? ").lower()
+if Q2 == "purple":
+    print(Win)
+    score += 1
+
+else:
+    print("{} purple".format(Lose))
+
+print()
+Q3 = input("What Colour is the Legendary rarity? ").lower()
+if Q3 == "gold" or Q3 == "yellow":
+    print(Win)
+    score += 1
+
+else:
+    print("{} gold".format(Lose))
+
+print()
+Q4 = input("How much people are in a fortnite match? ").lower()
+if Q4 == "100 players" or Q4 == "100":
+    print(Win)
+    score += 1
+
+else:
+    print("{} 100 players".format(Lose))
+#this tells the user that the questions after this will be more difficult
+print()
+print("That was easy im gonna increase the difficulty to medium level")
+print("your score is {} ".format(score))
+
+print()
+Q5 = input("What season (not chapter) was the combat shotgun added? ").lower()
+if Q5 == "season 9" or Q5 == "s9" or Q5 == "9":
+    print(Win)
+    score += 1
+
+else:
+    print("{} season 9".format(Lose))
+
+print()
+Q6 = input("What season (not chapter) was primal season? ").lower()
+if Q6 == "season 15" or Q6 == "s15" or Q6 == "15":
+    print(Win)
+    score += 1
+
+else:
+    print("{} season 15".format(Lose))
+
+print()
+Q7 = input("What season (not chapter) was flick lock added?").lower()
+if Q7 == "season 8" or Q7 == "s8" or Q7 == "8":
+    print(Win)
+    score += 1
+
+else:
+    print("{} season 8".format(Lose))
+#this tells the user that the questions after this will be more difficult
+print()
+print("That was easy im gonna increase the difficulty to hard level")
+print("your score is {} ".format(score))
+
+print()
+Q8 = input("What season (not chapter) was spider-man added? ").lower()
+if Q8 == "s19" or Q8 == "season 19" or Q8 == "19":
+    print(Win)
+    score += 1
+
+else:
+    print("{} season 19".format(Lose))
+
+print()
+Q9 = input("What is fortnite: battle royale release date? ").lower()
+if Q9 == "2017":
+    print(Win)
+    score += 1
+
+else:
+    print("{} 2017".format(Lose))
+
+print()
+Q10 = input("What is the most used pickaxe? ").lower()
+if Q10 == "the reaper" or Q10 == "reaper":
+    print(Win)
+    score += 1
+
+else:
+    print("{} the reaper pickaxe".format(Lose))
+
+print()
+#THis shows the user how many they got right
+if score == 0:
+    print("WOW! you got 0 you really dont know much about fortnite")
+else:
+
+    print("Good job you got {} out of 10 questions".format(score))
